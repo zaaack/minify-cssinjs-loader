c1d61bbc1988df2691464a88c9d8df49c8f7e689
zaaack <zaaack@qq.com>
2019-02-21 11:33:15 +0800

    0.1.4

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,8 +2,7 @@ import './types';
 declare type TagRule = string | RegExp | ((v: string) => boolean);
 export declare const defaultTagRules: TagRule[];
 export interface Options {
-    recast?: any;
     tagRules?: TagRule[];
 }
-declare function loader(content: any, map: any, meta: any): any;
+declare function loader(content: any, map: any, meta: any): string;
 export default loader;
